306dfdf2eefca25552ec792356ff83bb7a127470
Mondotosz <git@moncorp.ch>
2024-10-06 11:47:56 +0200

    Make RLE methods public

--- src/main/java/ch/heigvd/dai/algorithms/RLE.java
+++ src/main/java/ch/heigvd/dai/algorithms/RLE.java
@@ -2,10 +2,10 @@ package ch.heigvd.dai.algorithms;
 
 public class RLE extends LosslessAlgorithm {
   @Override
-  void compress(String archive, String output) {
+  public void compress(String archive, String output) {
   };
 
   @Override
-  void extract(String archive, String output) {
+  public void extract(String archive, String output) {
   };
 }
