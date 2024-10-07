0584fb960ce95bf9423aac2fb40d4972af36689d
Thynkon <thynkon@protonmail.com>
2024-10-07 17:57:32 +0200

    Compress and decompress tar files using Apache common-compress library

--- src/main/java/ch/heigvd/dai/commands/Root.java
+++ src/main/java/ch/heigvd/dai/commands/Root.java
@@ -1,9 +1,16 @@
 package ch.heigvd.dai.commands;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.concurrent.Callable;
 
+import org.apache.commons.compress.archivers.tar.TarArchiveOutputStream;
+
 import ch.heigvd.dai.algorithms.*;
+import ch.heigvd.dai.factories.LosslessAlgorithmFactory;
+import ch.heigvd.dai.wrappers.TarWrapper;
 import picocli.CommandLine;
 
 @CommandLine.Command(description = "A small CLI that compresses and deflates files.", version = "1.0.0", scope = CommandLine.ScopeType.INHERIT, mixinStandardHelpOptions = true)
@@ -24,14 +31,11 @@ public class Root implements Callable<Integer> {
 
   @CommandLine.Option(names = { "-o",
       "--output" }, description = "The output file or directory for both compress and deflate commands", required = true)
-  private String output;
+  private File output;
 
   public Integer call() {
     // TODO: Use factory in the future
-    LosslessAlgorithm compression_algorithm = switch (algorithm) {
-      case LZW -> new LZW();
-      case RLE -> new RLE();
-    };
+    LosslessAlgorithm compression_algorithm = LosslessAlgorithmFactory.make(algorithm);
 
     if (create && extract) {
       // TODO: throw specific exception
@@ -41,13 +45,35 @@ public class Root implements Callable<Integer> {
     if (create) {
       // TODO: for now, only the first file is archived is tmp!
       // In the future, only .tar files will be passed to compress()
-      compression_algorithm.compress(files[0].getAbsolutePath(), output);
+      // compression_algorithm.compress(files[0].getAbsolutePath(), output);
+
+      try (
+          OutputStream fos = new FileOutputStream(output);
+          TarArchiveOutputStream tar_stream = new TarArchiveOutputStream(fos);) {
+
+        for (File file : this.files) {
+          TarWrapper.add(tar_stream, file, "");
+        }
+
+        // write final tar file
+        tar_stream.finish();
+      } catch (IOException e) {
+        // TODO: handle exception
+      }
     }
 
     if (extract) {
+      if (this.files.length > 1) {
+        throw new IllegalArgumentException("Too many files passed to extract mode! Please, specify only one!");
+      }
       // TODO: for now, only the first file is extracted is tmp!
       // In the future, only .tar files will be passed to extract()
-      compression_algorithm.extract(files[0].getAbsolutePath(), output);
+      // compression_algorithm.extract(files[0].getAbsolutePath(), output);
+      try {
+        TarWrapper.extract(this.files[0], this.output);
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
     }
 
     return 0;
